6025264aacec8ab5883e959f6d96f069b3069428
spectrumgujranwala <spectrumgujranwala@gmail.com>
2026-06-12 14:24:39 +0500

    fix: remove ip binding from app.js

--- src/app.js
+++ src/app.js
@@ -43,6 +43,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
